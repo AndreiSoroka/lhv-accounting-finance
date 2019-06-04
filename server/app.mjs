7ac3ace4dbff6c423cc683af2381891ace923a41
andreisoroka <unickpro@gmail.com>
2019-06-05 00:18:05 +0300

    update express to mjs files

--- server/app.mjs
+++ server/app.mjs
@@ -1,13 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import logger from 'morgan';
 
-var app = express();
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+
+const app = express();
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,18 +20,18 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,4 +41,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+export default app;
